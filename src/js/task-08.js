73cc48f4edae08af31b25f4e58d6009eebb0a837
Stolbin <109080579+Stolbin@users.noreply.github.com>
2022-12-09 02:14:29 +0200

    Update task-08.js

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,21 @@
+const formRef = document.querySelector('.login-form');
+
+formRef.addEventListener('submit', validForm);
+
+function validForm(event) {
+
+    event.preventDefault();
+    const formElements = event.currentTarget.elements;
+    const email = formElements.email.value;
+    const password = formElements.password.value;
+    if (email.length === 0 || password.length === 0) {
+        alert('Будь ласка, заповніть всі поля');
+        return;
+    }
+        const formData = {
+            email,
+            password,
+        }
+        formRef.reset();
+} 
 
