5cbb1eb96f3b689ca8a247fe4d80e170fcac7541
Stolbin <109080579+Stolbin@users.noreply.github.com>
2022-12-09 04:24:18 +0200

    Update task-10.js

--- src/js/task-10.js
+++ src/js/task-10.js
@@ -1,3 +1,34 @@
+const inputScoreBoxsRef = document.querySelector('.controls input');
+const btnCreateRef = document.querySelector('[data-create]');
+const btnDestroyRef = document.querySelector('[data-destroy]');
+const boxRef = document.querySelector('#boxes');
+
+btnCreateRef.addEventListener('click', createBoxes);
+
+
+function getAmount() {
+  const amount = inputScoreBoxsRef.value;
+  return amount;
+};
+
+
+function createBoxes(amount) {
+  const defaultSizeBox = '30px';
+  const defaultBox = `<div style = "width: ${defaultSizeBox}px; height: ${defaultSizeBox}px></div>`;
+  for (let i = 0; i < amount; i+=i) {
+    boxes += defaultBox;
+    defaultBox = `<div style = "width: ${defaultSizeBox += 10}px; height: ${defaultSizeBox += 10}px></div>`;
+    boxes.style.backgroundColor = getRandomHexColor();
+  }
+  boxRef.insertAdjacentHTML('afterbegin', boxes)
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+btnDestroyRef.addEventListener('click', destroyBoxes);
+
+function destroyBoxes() {
+  btnCreateRef.reset();
 }
