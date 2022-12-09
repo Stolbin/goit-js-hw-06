7d1c5136eb574edd07709fe22b05ecf3a8cb93ce
Stolbin <109080579+Stolbin@users.noreply.github.com>
2022-12-10 01:24:30 +0200

    Update task-10.js

--- src/js/task-10.js
+++ src/js/task-10.js
@@ -3,32 +3,23 @@ const btnCreateRef = document.querySelector('[data-create]');
 const btnDestroyRef = document.querySelector('[data-destroy]');
 const boxRef = document.querySelector('#boxes');
 
-
-console.log(inputScoreBoxsRef);
-console.log(btnCreateRef);
-console.log(btnDestroyRef);
-console.log(boxRef);
-
 function getAmount() {
   const amount = inputScoreBoxsRef.value;
   return amount;
 };
 
-
 function createBoxes(amount) {
   const size = 30;
-  let allBoxes = [];
-  for (let i = 0; i < amount; i += 1) {
+  const allBoxes = [];
+  for (const i = 0; i < amount; i += 1) {
     const defaultBox = document.createElement('div');
+    allBoxes.push(defaultBox);
     defaultBox.style.width = `(${size} + 10)px`;
     defaultBox.style.height = `(${size} + 10)px`;
     defaultBox.style.background = getRandomHexColor();
-    allBoxes.push(defaultBox);
-    boxRef.append(...allBoxes);
-    return boxRef;
+    return allBoxes;
   }
-    // boxRef.insertAdjacentHTML('afterbegin', allBoxes);
-
+  boxRef.append(...allBoxes);
   };
   
   function getRandomHexColor() {
@@ -47,21 +38,4 @@ btnDestroyRef.addEventListener('click', destroyBoxes);
 function destroyBoxes() {
   boxRef.innerHTML = "";
   inputScoreBoxsRef.value = "";
-
-};  
-
-
-
-
-  
-  
-  // const size = 30;
-
-  //   const str = `<div style="width=${size}px; heigth=${size}px"`;
-  //   const multiBoxes = [];
-
-  //   for (const i = 0; i < amount; i += 1) {
-  //     multiBoxes += str;
-  //     str = `<div style="width=(${size} + 10)px; heigth=${size} + 10px"`
-  //   }
-  //   boxRef.insertAdjacentHTML('afterbegin', multiBoxes);
+}; 
