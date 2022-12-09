22ffe399c550aee6591e4ced71fb24ec76e1b23d
Stolbin <109080579+Stolbin@users.noreply.github.com>
2022-12-10 01:12:39 +0200

    Update task-10.js

--- src/js/task-10.js
+++ src/js/task-10.js
@@ -1,41 +1,67 @@
 const inputScoreBoxsRef = document.querySelector('#controls input');
-inputScoreBoxsRef.id = 'input';
 const btnCreateRef = document.querySelector('[data-create]');
 const btnDestroyRef = document.querySelector('[data-destroy]');
 const boxRef = document.querySelector('#boxes');
+
+
+console.log(inputScoreBoxsRef);
+console.log(btnCreateRef);
+console.log(btnDestroyRef);
+console.log(boxRef);
 
 function getAmount() {
   const amount = inputScoreBoxsRef.value;
   return amount;
 };
 
+
 function createBoxes(amount) {
-  const size = '30';
+  const size = 30;
   let allBoxes = [];
-    for (let i = 0; i < amount; i += i) { 
-      allBoxes += defaultBox; 
-      let defaultBox = document.createElement('div');
-      defaultBox.style.width = `(${size})px`;
-      defaultBox.style.height = `(${size})px`;
-      defaultBox.style.backgroundColor = getRandomHexColor();
-      defaultBox.style.width = `(${size} + 10)px`;
-      defaultBox.style.height = `(${size} + 10)px`;
-      defaultBox.push(defaultBox);
-      allBoxes.append(...defaultBox);
-      return;
-    }
-    boxRef.insertAdjacentHTML('afterbegin', allBoxes);
-};
+  for (let i = 0; i < amount; i += 1) {
+    const defaultBox = document.createElement('div');
+    defaultBox.style.width = `(${size} + 10)px`;
+    defaultBox.style.height = `(${size} + 10)px`;
+    defaultBox.style.background = getRandomHexColor();
+    allBoxes.push(defaultBox);
+    boxRef.append(...allBoxes);
+    return boxRef;
+  }
+    // boxRef.insertAdjacentHTML('afterbegin', allBoxes);
 
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-};
+  };
+  
+  function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  };
 
-btnCreateRef.addEventListener('click', createBoxes);
+function handleOnClickBtnCreateBoxes() {
+  getAmount();
+  createBoxes();
+}
+
+inputScoreBoxsRef.addEventListener('change', getAmount);
+btnCreateRef.addEventListener('click', handleOnClickBtnCreateBoxes);
 btnDestroyRef.addEventListener('click', destroyBoxes);
 
 function destroyBoxes() {
   boxRef.innerHTML = "";
   inputScoreBoxsRef.value = "";
 
-};
+};  
+
+
+
+
+  
+  
+  // const size = 30;
+
+  //   const str = `<div style="width=${size}px; heigth=${size}px"`;
+  //   const multiBoxes = [];
+
+  //   for (const i = 0; i < amount; i += 1) {
+  //     multiBoxes += str;
+  //     str = `<div style="width=(${size} + 10)px; heigth=${size} + 10px"`
+  //   }
+  //   boxRef.insertAdjacentHTML('afterbegin', multiBoxes);
