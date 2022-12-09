fa2cd046b83471b1b6b6f4ad8ec100abbb4cf428
Stolbin <109080579+Stolbin@users.noreply.github.com>
2022-12-09 02:52:46 +0200

    Update task-09.js

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,17 @@
+const bodyBackground = document.querySelector('body');
+const output = document.querySelector('.color');
+const btnTextColor = document.querySelector('.change-color');
+
+bodyBackground.addEventListener('click', changeColorOnClick);
+
+function changeColorOnClick() {
+  const colorResult = getRandomHexColor();
+  output.textContent = colorResult;
+  output.style.fontSize = '60px';
+  bodyBackground.style.backgroundColor = colorResult;
+  btnTextColor.style.color = colorResult;
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
