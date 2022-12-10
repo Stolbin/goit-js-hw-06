857a389b149806519ded0eeddfda3bf43b82caf8
Stolbin <109080579+Stolbin@users.noreply.github.com>
2022-12-10 03:07:51 +0200

    Update task-10.js

--- src/js/task-10.js
+++ src/js/task-10.js
@@ -1,41 +1,49 @@
-const inputScoreBoxsRef = document.querySelector('#controls input');
+const inputScoreBoxesRef = document.querySelector('input');
 const btnCreateRef = document.querySelector('[data-create]');
 const btnDestroyRef = document.querySelector('[data-destroy]');
 const boxRef = document.querySelector('#boxes');
 
-function getAmount() {
-  const amount = inputScoreBoxsRef.value;
-  return amount;
+function getAmount(event) {
+  createBoxes(Number(inputScoreBoxesRef.value));
 };
 
 function createBoxes(amount) {
   const size = 30;
-  const allBoxes = [];
-  for (const i = 0; i < amount; i += 1) {
-    const defaultBox = document.createElement('div');
-    allBoxes.push(defaultBox);
-    defaultBox.style.width = `(${size} + 10)px`;
-    defaultBox.style.height = `(${size} + 10)px`;
-    defaultBox.style.background = getRandomHexColor();
-    return allBoxes;
+  let allBoxes = '';
+  for (let i = 1; i < amount; i += 1) {
+    let box = document.createElement(`div`);
+    allBoxes += box;
+    box.style.width = `((${size} + i) + 10)px`;
+    box.style.height = `((${size} + i) + 10)px`;
+    box.style.background = getRandomHexColor();
   }
-  boxRef.append(...allBoxes);
+  boxRef.insertAdjacentHTML('afterbegin', allBoxes);
+};
+  
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
   };
   
-  function getRandomHexColor() {
-    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-  };
-
-function handleOnClickBtnCreateBoxes() {
+  function handleOnClickBtnCreateBoxes(event) {
   getAmount();
   createBoxes();
+  alertOnInput();
 }
 
-inputScoreBoxsRef.addEventListener('change', getAmount);
+inputScoreBoxesRef.addEventListener('change', getAmount);
 btnCreateRef.addEventListener('click', handleOnClickBtnCreateBoxes);
 btnDestroyRef.addEventListener('click', destroyBoxes);
 
 function destroyBoxes() {
   boxRef.innerHTML = "";
-  inputScoreBoxsRef.value = "";
+  inputScoreBoxesRef.value = "";
 }; 
+
+function alertOnInput (event) {
+  if (inputScoreBoxesRef.value > 100) {
+    alert('Будь ласка, введіть число меньше 100');
+    destroyBoxes();
+    createBoxes();
+    return;
+  }
+};
